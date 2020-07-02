02799ed1e2ea9f27923a553b30619c3afb317c46
zwtian80 <zwtian211@yahoo.com>
2020-07-02 18:31:52 -0400

    First commit

---
+++ Net012Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net012Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
