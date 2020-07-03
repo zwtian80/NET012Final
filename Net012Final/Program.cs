1e204b9f1c3d49f8b6259724b74c69c210f147dc
zwtian80 <zwtian211@yahoo.com>
2020-07-02 20:28:24 -0400

    final

--- Net012Final/Program.cs
+++ Net012Final/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+In the space provided on this page write a program using these instructions:
+1.    Create two classes, one called ‘Adult’ and one called ‘Child’ with the following string properties:
+a.    First Name
+b.    Last Name
+2.    Populate the first and last name properties in both the Adult and Child classes.
+3.    Create a single array to hold both the Adult and Child classes.
+4.    Populate the array with one Adult class and one Child class.  
+5.    Use the array in a loop and print the Adult and Child first and last name properties to the console.
+*/
+
+using System;
 
 namespace Net012Final
 {
@@ -6,7 +17,41 @@ namespace Net012Final
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] wholeArray = new string[100];
+
+            Adult myAdult = new Adult();
+            myAdult.FirstName = "Joe";
+            myAdult.LastName = "Smith";
+
+            Child myChild = new Child();
+            myChild.FirstName = "Mary";
+            myChild.LastName = "Wood";
+
+            wholeArray[0] = (string)(myAdult.FirstName+ ' ' + myAdult.LastName);
+            wholeArray[1] = (string)(myChild.FirstName + ' ' + myChild.LastName);
+
+            for (int i = 0; i < wholeArray.Length; i++)
+            {
+                if (wholeArray[i] != null)
+                    Console.WriteLine(wholeArray[i]);
+            }
+
         }
     }
+
+    public class Adult
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    public class Child
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 }
+
+
+
